afe579e7627d7bca259606fdb8e178cf8eaffdcf
JOHNS JOSEPH <35776080+DEVELOPER-IN-PROGRESS@users.noreply.github.com>
2020-09-04 18:22:52 +0530

    Update snake_game_final.c

--- snake_game_final.c
+++ snake_game_final.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<windows.h>
-#include<direct.h>
+#include<windows.h> 
+#include<unistd.h>
 void gotoxy(short ,short ) ;
 void welcome(void);
 int i ,j ;
+
+struct coorainate
+{
+   int x ; int y ; char d ;
+} ;
+
+typedef struct  coordinate  dir ;
+
 
 int main(int argc , char *argv[])
 {
@@ -37,12 +45,8 @@ void welcome (void)
 {
   for(j=0 ;j<30 ;j++)
     for(i=0;i<80 ;i++)
-    {   
-      (i<41)?system("color 0A"):system("color 01");
-        gotoxy(30+i,5+j);
-        printf("*"); 
-    }
-       printf("\n") ;
+       {gotoxy(30+i,50 + j); printf("*"); sleep(0.42 ); }
+      printf("\n") ;
     // gotoxy(31,5);printf("*");
      //gotoxy(32,5);printf("*");
      //gotoxy(33,5);printf("*");
